e2fe3421be301b138a3f189ff50355cda0d9f3b2
manish <gkrajoriya3@gmail.com>
2024-11-26 17:33:54 +0530

    ..

--- src/components/addMemberCard.tsx
+++ src/components/addMemberCard.tsx
@@ -1,4 +1,4 @@
-"use client"
+
 import React from 'react'
 import {
   Popover,
@@ -7,23 +7,25 @@ import {
 } from "@/components/ui/popover"
 
 import PlanForm from './forms/planForm'
-import { useRouter } from 'next/navigation'
+
 import Image from 'next/image'
+import Link from 'next/link'
 
 
 const AddMemberCard = () =>{
-    const router = useRouter();
+    
     return(
       <div>
         <div className="grid grid-cols-1 gap-4 mb-6">
     {/* <!-- Card items --> */}
-        <button title='Add Student' onClick={() =>router.push("/addMember") }>
+    <Link href={'/addMember'}>
+        <button title='Add Student' >
         <div className="flex items-center p-4 bg-white rounded-lg shadow">
             <Image width={6} height={6} src="/add.png" alt="Reels Icon" className="w-6 h-6 mr-2" />
             <span>Add Student</span>
         </div>
        </button>
-
+     </Link>
     {/* <div className="flex items-center p-4 bg-white rounded-lg shadow">
       <img src="add.png" alt="Reels Icon" className="w-6 h-6 mr-2" />
       <span>Add Student</span>
